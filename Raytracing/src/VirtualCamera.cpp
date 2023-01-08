1f7e3f4a973b2d86ab30c78a32ab8f9d9edb88b1
EmiNiemand <Szymon_PL@live.com>
2023-01-08 19:26:08 +0100

    fix view points calculation

--- Raytracing/src/VirtualCamera.cpp
+++ Raytracing/src/VirtualCamera.cpp
@@ -9,25 +9,24 @@ VirtualCamera::VirtualCamera(const Vector3 &position) : position(position) {
     CalculateViewPoints();
 }
 
+// TODO: check in geogebra
 void VirtualCamera::CalculateViewPoints() {
     Vector4 lookingDirection(position * -1);
-    Vector4 lookingDirectionNormalized = lookingDirection;
-    lookingDirectionNormalized.normalize();
     Vector4 pointDistance(lookingDirection / 2);
-    Vector4 viewDistance(lookingDirection * ViewRatio);
+    Vector4 viewDistance(lookingDirection / 2 * ViewRatio);
 
     Matrix4x4 rotation;
     rotation.LoadIdentity();
-    rotation.SetRotationAxis(90, {lookingDirection.x, 0, 0, 1});
+    rotation.SetRotationX(90);
     pointDistance = rotation * pointDistance;
     rotation.LoadIdentity();
-    rotation.SetRotationAxis(45, {0, 0, lookingDirection.z, 1});
+    rotation.SetRotationZ(45);
     pointDistance = rotation * pointDistance;
 
     view[0] = Vector3(lookingDirection - viewDistance + pointDistance);
     for(int i = 1; i < 4; i++){
         rotation.LoadIdentity();
-        rotation.SetRotationAxis(90, {0, 0, lookingDirection.z, 1});
+        rotation.SetRotationZ(90);
         pointDistance = rotation * pointDistance;
         view[i] = Vector3(lookingDirection - viewDistance + pointDistance);
     }
